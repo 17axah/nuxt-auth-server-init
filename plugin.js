d94ec3a5d74b5c4f96b88f24445f768c726a1bc2
17axaH <17axaH@mail.ru>
2021-11-27 00:56:03 +0000

    corrections

--- plugin.js
+++ plugin.js
@@ -8,10 +8,20 @@ function actionDeclared(name, actions) {
 
 function loginDecorator(fn, context) {
   return async function (...args) {
-    const result = await fn.apply(this, args)
+    const redirect = context.$auth.redirect
+
+    context.$auth.redirect = () => {}
+
+    const result = await fn.apply(this, args).finally(() => {
+      context.$auth.redirect = redirect
+    })
 
     if (actionDeclared(ACTION_NAME, context.store._actions)) {
       await context.store.dispatch(ACTION_NAME, context)
+    }
+
+    if (context.$auth.options.watchLoggedIn) {
+      context.$auth.redirect('home')
     }
 
     return result
